bb8d8778e0403dac502592839498926e333ddcb8
renren-abc <nba.16.ren@gmail.com>
2016-10-07 17:42:40 +0900

    TouchManager変更

--- Assets/Scripts/Manager/TouchManager.cs
+++ Assets/Scripts/Manager/TouchManager.cs
@@ -7,15 +7,20 @@ public static class TouchManager
 {
     private static Vector3 m_TouchPosition = Vector3.zero;
     private static Vector3 m_PreviousPosition = Vector3.zero;
+    private static Vector2 m_FirstTouchPosition = Vector2.zero;
 
     // タッチ情報を取得(エディタと実機を考慮)
     // <returns>タッチ情報。タッチされていない場合は null</returns>
 
-    public static TouchInfo GetTouchInfo()
+    public static TouchInfo GetTouchInfo(int touchCount)
     {
         if (Application.isEditor)
         {
-            if (Input.GetMouseButtonDown(0)) { return TouchInfo.Began; }
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_FirstTouchPosition = Input.mousePosition;
+                return TouchInfo.Began;
+            }
             if (Input.GetMouseButton(0)) { return TouchInfo.Moved; }
             if (Input.GetMouseButtonUp(0)) { return TouchInfo.Ended; }
         }
@@ -23,7 +28,9 @@ public static class TouchManager
         {
             if (Input.touchCount > 0)
             {
-                return (TouchInfo)((int)Input.GetTouch(0).phase);
+                Touch touch = Input.GetTouch(0);
+                m_FirstTouchPosition = touch.position;
+                return (TouchInfo)((int)Input.GetTouch(touchCount).phase);
             }
         }
         return TouchInfo.None;
@@ -32,11 +39,11 @@ public static class TouchManager
     // タッチポジションを取得(エディタと実機を考慮)
     // <returns>タッチポジション。タッチされていない場合は (0, 0, 0)</returns>
 
-    public static Vector3 GetTouchPosition()
+    public static Vector3 GetTouchPosition(int touchCount)
     {
         if (Application.isEditor)
         {
-            TouchInfo touch = TouchManager.GetTouchInfo();
+            TouchInfo touch = TouchManager.GetTouchInfo(touchCount);
             if (touch != TouchInfo.None) { return Input.mousePosition; }
         }
         else
@@ -54,11 +61,11 @@ public static class TouchManager
 
     // 前回のタッチポジションを取得(エディタと実機を考慮)
 
-    public static Vector3 GetDeltaPosition()
+    public static Vector3 GetDeltaPosition(int touchCount)
     {
         if (Application.isEditor)
         {
-            TouchInfo info = TouchManager.GetTouchInfo();
+            TouchInfo info = TouchManager.GetTouchInfo(touchCount);
             if (info != TouchInfo.None)
             {
                 Vector3 currentPosition = Input.mousePosition;
@@ -80,21 +87,47 @@ public static class TouchManager
         return Vector3.zero;
     }
 
+    //　最初にタッチした地点からの移動距離
+    //　X座標
+    public static float GetTouchMoveDistanceX(int touchCount)
+    {
+        //現在のタッチのポジションを取得
+        float touchPosX = GetTouchPosition(touchCount).x;
+
+        //距離を算出
+        float distance = touchPosX - m_FirstTouchPosition.x;
+
+        return distance;
+    }
+
+    //　最初にタッチした地点からの移動距離
+    //　Y座標
+    public static float GetTouchMoveDistanceY(int touchCount)
+    {
+        //現在のタッチのポジションを取得
+        float touchPosY = GetTouchPosition(touchCount).y;
+
+        //距離を算出
+        float distance = touchPosY - m_FirstTouchPosition.y;
+
+        return distance;
+    }
+
     // タッチワールドポジションを取得(エディタと実機を考慮)
     // <param name='camera'>カメラ</param>
     // <returns>タッチワールドポジション。タッチされていない場合は (0, 0, 0)</returns>
 
-    public static Vector3 GetTouchWorldPosition(Camera camera)
+    public static Vector3 GetTouchWorldPosition(Camera camera, int touchCount)
     {
-        return camera.ScreenToWorldPoint(GetTouchPosition());
+        return camera.ScreenToWorldPoint(GetTouchPosition(touchCount));
     }
 
     // Raycast
-    public static GameObject GetRaycastHitObject()
+    public static GameObject GetRaycastHitObject(int touchCount)
     {
         RaycastHit hit;
 
-        Ray ray = Camera.main.ScreenPointToRay(TouchManager.GetTouchPosition());
+        Ray ray = Camera.main.ScreenPointToRay(TouchManager.GetTouchPosition(touchCount));
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
