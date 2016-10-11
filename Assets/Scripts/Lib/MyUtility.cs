413b9319b63eee8d849c157aaebbc81106558a6c
koji526develop <koji@KojiOkada-no-MacBook-Pro.local>
2016-10-11 23:37:01 +0900

    MainScene修正

---
+++ Assets/Scripts/Lib/MyUtility.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyUtility : MonoBehaviour 
+{
+	public static void CreateDirectionalLight()
+	{
+		GameObject lightObj = new GameObject ("DirectionalLight");
+		lightObj.transform.Rotate(new Vector3 (50, -30, 0));
+		Light light = lightObj.AddComponent<Light> ();
+		light.type = LightType.Directional;
+	}
+
+	public static void CreateCamera(string name, Transform _parent = null)
+	{
+		GameObject cameraObj = new GameObject (name);
+		Camera camera = cameraObj.AddComponent<Camera> ();
+
+		camera.transform.position = new Vector3 (0, 1, -10);
+
+		cameraObj.AddComponent<GUILayer> ();
+		cameraObj.AddComponent<FlareLayer> ();
+		cameraObj.AddComponent<AudioListener> ();
+
+		if(_parent) cameraObj.transform.SetParent (_parent);
+	}
+
+	public static void CreateUICanvas(Transform _parent)
+	{
+		GameObject uiObj = Instantiate(Resources.Load("Prefabs/UI") as GameObject);
+		uiObj.name = "UICanvas";
+		uiObj.transform.SetParent (_parent);
+	}
+
+	public static void CreateEventSystem(Transform _parent)
+	{
+		GameObject eventSystemObj = Instantiate(Resources.Load("Prefabs/EventSystem") as GameObject);
+		eventSystemObj.name = "EventSystem";
+		eventSystemObj.transform.SetParent (_parent);
+	}
+}
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class GameManager : MonoBehaviour 
+{
+	// ゲーム開始時
+	void Awake()
+	{
+		// Light作成
+		MyUtility.CreateDirectionalLight ();
+
+		//UIManager作成
+		Transform UIManager = new GameObject ("UIManager").transform;
+
+		// UICanvas作成
+		MyUtility.CreateUICanvas (UIManager);
+
+		// UIカメラ作成
+		MyUtility.CreateCamera ("UICamera", UIManager);
+
+		// イベントシステム作成
+		MyUtility.CreateEventSystem (UIManager);
+
+		//BattleManager作成
+		Transform battleManager = new GameObject ("BattleManager").transform;
+
+		// バトル用のカメラ作成
+		MyUtility.CreateCamera ("BattleCamera", battleManager);
+
+		// プレイヤーオブジェクト作成
+		GameObject playerObj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		playerObj.name = "Player";
+		playerObj.AddComponent<Player> ();
+		playerObj.transform.SetParent (battleManager);
+	}
+}
