5a9109085f1513494211b471a8a280e5685ba1fc
renren-abc <nba.16.ren@gmail.com>
2016-10-14 15:53:20 +0900

    シーン遷移作成

---
+++ Assets/Scripts/Manager/TitleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        TouchInfo touch = TouchManager.GetTouchInfo(0);
+        if(touch == TouchInfo.Began)
+        {
+            SceneChanger sChange = new SceneChanger();
+            sChange.ChangeToMenu();
+        }
+    }
+}
