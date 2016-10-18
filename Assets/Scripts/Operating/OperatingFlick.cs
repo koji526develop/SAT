23a25d84c21bca0113409a92cf547746114e5b66
renren-abc <nba.16.ren@gmail.com>
2016-10-18 18:58:39 +0900

    操作説明画面更新！

--- Assets/Scripts/Operating/OperatingFlick.cs
+++ Assets/Scripts/Operating/OperatingFlick.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OperatinFlick : MonoBehaviour
+public class OperatingFlick : MonoBehaviour
 {
-    public Sprite[] m_OperatingSprite = new Sprite[6];
+    Sprite[] m_OperatingSprite = new Sprite[MAX_SPRITE_NUM];
     SpriteRenderer m_DisplaySpriteRenderer;
-    private int m_DisplaySpriteNum;
+    int m_DisplaySpriteNum;
+    const int MAX_SPRITE_NUM = 6;
+
+    public int DisplayNum
+    {
+        get
+        {
+            return m_DisplaySpriteNum;
+        }
+    }
+
     readonly int MAX_FLICK_X = Screen.width*1/4;
 
     void Start()
     {
         m_DisplaySpriteNum = 0;
+        for (int i = 1; i < MAX_SPRITE_NUM+1; i++) 
+        {
+            m_OperatingSprite[i-1] = Resources.Load<Sprite>("Image/operating"+i.ToString());
+        }
     }
 
     void Update()
     {
-        Debug.Log(MAX_FLICK_X);
-        Debug.Log(TouchManager.GetTouchMoveDistanceX(0));
-
         TouchInfo touch = TouchManager.GetTouchInfo(0);
 
         if (touch == TouchInfo.Ended)
         {
-            if (TouchManager.GetTouchMoveDistanceX(0) > MAX_FLICK_X)
+            if (TouchManager.GetTouchMoveDistanceX(0) < -MAX_FLICK_X)
             {
                 m_DisplaySpriteNum++;
                 ChangeSprite();
             }
-            else if (TouchManager.GetTouchMoveDistanceX(0) < MAX_FLICK_X)
+            else if (TouchManager.GetTouchMoveDistanceX(0) > MAX_FLICK_X)
             {
                 m_DisplaySpriteNum--;
                 ChangeSprite();
@@ -42,12 +53,11 @@ public class OperatinFlick : MonoBehaviour
         {
             m_DisplaySpriteNum = 0;
         }
-        else if (m_DisplaySpriteNum > 5)
+        else if (m_DisplaySpriteNum >= m_OperatingSprite.Length)
         {
-            m_DisplaySpriteNum = 5;
+            m_DisplaySpriteNum = m_OperatingSprite.Length-1;
         }
         m_DisplaySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         m_DisplaySpriteRenderer.sprite = m_OperatingSprite[m_DisplaySpriteNum];
-        Debug.Log("スプライト変更");
     }
 }
