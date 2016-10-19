575f037bf9223a01bc4875f600e3828460bfefe8
koji526develop <koji@KojiOkada-no-MacBook-Pro.local>
2016-10-19 13:37:39 +0900

    UIをScriptで作成できるようにしました

---
+++ Assets/CanvasScalerManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasScalerManager : MonoBehaviour {
+	CanvasScaler m_canvasScaler;
+	// Use this for initialization
+	void Start () {
+		m_canvasScaler = GetComponent<CanvasScaler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_canvasScaler.referenceResolution = new Vector2 (Screen.width, Screen.height);
+	}
+}
---
+++ Assets/Scripts/Manager/SelectManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectManager : MonoBehaviour 
+{
+	void Awake () 
+	{
+		// ライト作成
+		//MyUtility.CreateDirectionalLight ();
+
+		// MAINカメラ作成
+		//Camera mainCamera = MyUtility.CreateCamera ("MainCamera");
+	}
+
+	void Update () 
+	{
+	
+	}
+}
