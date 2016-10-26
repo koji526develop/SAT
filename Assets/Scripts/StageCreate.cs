4dc8b56394bc60d3214bef71f5527f50893b7fe8
noan23435 <noan23435@gmail.com>
2016-10-26 15:10:38 +0900

    ステージSprite

--- Assets/Scripts/StageCreate.cs
+++ Assets/Scripts/StageCreate.cs
@@ -5,9 +5,15 @@ public class StageCreate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject obj=new GameObject ("Floor");
-		SpriteRenderer spr= obj.AddComponent<SpriteRenderer> ();
-		spr.sprite = Resources.Load ("Resources/Image/masu", typeof(Sprite))as Sprite;
+		
+			GameObject obj= new GameObject ("Floor");
+			SpriteRenderer spr = obj.AddComponent<SpriteRenderer> ();
+			spr.sprite = Resources.Load ("Image/masu", typeof(Sprite))as Sprite;
+			//obj.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+			obj.transform.eulerAngles = new Vector3 (90.0f, 0.0f, 0.0f);
+			obj.transform.localScale = new Vector3 (2.0f, 1.0f, 1.0f);
+
+
 
 	}
 	
