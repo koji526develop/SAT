a46a09d6398ef82114babbb736ed453a44383097
noan23435 <noan23435@gmail.com>
2016-10-28 13:18:47 +0900

    ステージ見た目完成

--- Assets/Scripts/StageCreate.cs
+++ Assets/Scripts/StageCreate.cs
@@ -2,17 +2,23 @@
 using System.Collections;
 
 public class StageCreate : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		
-			GameObject obj= new GameObject ("Floor");
-			SpriteRenderer spr = obj.AddComponent<SpriteRenderer> ();
-			spr.sprite = Resources.Load ("Image/masu", typeof(Sprite))as Sprite;
-			//obj.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
-			obj.transform.eulerAngles = new Vector3 (90.0f, 0.0f, 0.0f);
-			obj.transform.localScale = new Vector3 (2.0f, 1.0f, 1.0f);
+		for (int i = 0; i < 15; i++) 
+		{
+			for (int j = 0; j < 15; j++) 
+			{
+				GameObject obj = new GameObject ("Floor");
+				SpriteRenderer spr = obj.AddComponent<SpriteRenderer> ();
+				spr.sprite = Resources.Load ("Image/masu", typeof(Sprite))as Sprite;
+				obj.transform.position = new Vector3 (-7.0f + (i * 1), 0.0f, -5.0f+(j*1));
+				obj.transform.eulerAngles = new Vector3 (90.0f, 0.0f, 0.0f);
+				obj.transform.localScale = new Vector3 (2.0f, 1.0f, 1.0f);
+			}
 
+		}
 
 
 	}
