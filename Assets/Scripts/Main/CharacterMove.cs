b0040f246e3697db02ff14c439cddccf8c0d22f7
noan23435 <noan23435@gmail.com>
2016-10-31 13:35:07 +0900

    ステージ

--- Assets/Scripts/Main/CharacterMove.cs
+++ Assets/Scripts/Main/CharacterMove.cs
@@ -1,84 +1,105 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterMove :  State<Character> {
+public class CharacterMove :  State<Character>
+{
 
 	bool m_CharacterTouch;
 
-	public CharacterMove(Character _instance) : base(_instance) {}
+	public CharacterMove (Character _instance) : base (_instance)
+	{
+	}
 
-	public override void Enter() 
+	public override void Enter ()
 	{
 		m_CharacterTouch = false;
 	}
 
-	public override void Update()
+	public override void Update ()
 	{
-		m_instance.transform.position += new Vector3 (0.005f,0,0);
+
+		if (m_instance.status.PlayerID == 1)m_instance.transform.position += new Vector3 (0.005f, 0, 0);
+		else m_instance.transform.position -= new Vector3 (0.005f, 0, 0);
 
 		TouchInfo touch = TouchManager.GetTouchInfo (0);
 
 		/***********************************************
 		// キャラクターのタッチ判定
 		***********************************************/
-		if(touch == TouchInfo.Began)
-		{
+		if (touch == TouchInfo.Began) {
 			GameObject characterObj = TouchManager.GetRaycastHitObject (m_instance.MainCamera, 0);
-			if (characterObj != null) 
-			{
+			if (characterObj != null) {
 				m_CharacterTouch = true;
-			} 
-			else
-			{
+			} else {
 				m_CharacterTouch = false;
 			}
 		}
 
-		if(touch == TouchInfo.Moved)
+		if (touch == TouchInfo.Moved) 
 		{
-			if (m_CharacterTouch) 
+			if (m_CharacterTouch&& !NotCharacterRota ()) 
 			{
-				if (TouchManager.GetTouchMoveDistanceY (0) > 50.0f)
-				{	
-					iTween.RotateBy(m_instance.gameObject, new Vector3(1/4.0f,0,0),1.0f);
-					m_instance.ChangeState (Character.CharacterState.Rotate);
-				} 
-				else if (TouchManager.GetTouchMoveDistanceY (0) < -50.0f) 
-				{
-					iTween.RotateBy(m_instance.gameObject, new Vector3(-1/4.0f,0,0),1.0f);
-					m_instance.ChangeState (Character.CharacterState.Rotate);
+				int DirectionRote;
+				if (m_instance.status.PlayerID == 1) {
+					DirectionRote = 1;
+				} else {
+					DirectionRote = -1;
 				}
+
+					if (TouchManager.GetTouchMoveDistanceY (0) > 50.0f) 
+					{	
+					iTween.RotateBy (m_instance.gameObject, new Vector3 (0, (-1*DirectionRote) / 4.0f, 0), 1.0f);
+						m_instance.ChangeState (Character.CharacterState.Rotate);
+					} 
+					else if (TouchManager.GetTouchMoveDistanceY (0) < -50.0f) 
+					{
+					iTween.RotateBy (m_instance.gameObject, new Vector3 (0, (1*DirectionRote) / 4.0f, 0), 1.0f);
+						m_instance.ChangeState (Character.CharacterState.Rotate);
+					}
 			}
-		}
-		/***********************************************
+			/***********************************************
 		// 全てのキャラクター同士の衝突判定
 		***********************************************/
-		GameObject[] gameobject = GameObject.FindGameObjectsWithTag("Character");
+			GameObject[] gameobject = GameObject.FindGameObjectsWithTag ("Character");
 
-		for(int i=0;i<gameobject.Length;i++)
-		{
-			if (m_instance.gameObject == gameobject [i])
-				continue;
+			for (int i = 0; i < gameobject.Length; i++) {
+			
+				if (m_instance.gameObject == gameobject [i])
+					continue;
 
-			if(gameobject[i].CompareTag("Character") && IsHit(m_instance.gameObject, gameobject[i],m_instance.status.attackDistance))
-			{
-				int HitObjID = gameobject [i].GetComponent<Character> ().status.PlayerID;
-				if(m_instance.status.PlayerID != HitObjID)
-				{
-				m_instance.ChangeState (Character.CharacterState.Attack);
-				Debug.Log("当たったー");
+				if (gameobject [i].CompareTag ("Character") && IsHit (m_instance.gameObject, gameobject [i], m_instance.status.attackDistance)) {
+					int HitObjID = gameobject [i].GetComponent<Character> ().status.PlayerID;
+					if (m_instance.status.PlayerID != HitObjID) {
+						m_instance.ChangeState (Character.CharacterState.Attack);
+						Debug.Log ("当たったー");
+					}
 				}
 			}
 		}
 	}
 
+	bool NotCharacterRota ()
+	{
+		if (m_instance.status.PlayerID == 1) {
+			if (m_instance.transform.position.x >= 0.0f) {
+				return true;
+			}
+		} else {
+			if (m_instance.transform.position.x <= 0.0f) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	// キャラクターの衝突判定
-	bool IsHit(GameObject obj1 , GameObject obj2, float distance)
+	bool IsHit (GameObject obj1, GameObject obj2, float distance)
 	{
 		return Mathf.Abs (obj1.transform.position.x - obj2.transform.position.x) < distance;
 	}
 
-	public override void Exit() 
+	public override void Exit ()
 	{
 		
 	}
--- Assets/Scripts/StageCreate.cs
+++ Assets/Scripts/StageCreate.cs
@@ -7,21 +7,26 @@ public class StageCreate : MonoBehaviour {
 	void Start () {
 
 		Transform FloorFile = new GameObject ("FloorFile").transform;
+		//FloorFile.transform.Rotate(new Vector3(1,0,0),-90);
 
 
-		for (int i = 0; i < 15; i++) 
+		for (int i = 0; i < 6; i++) 
 		{
-			for (int j = 0; j < 15; j++) 
+			for (int j = 0; j < 9; j++) 
 			{
-				GameObject obj = new GameObject ("Floor");
-				SpriteRenderer spr = obj.AddComponent<SpriteRenderer> ();
-				spr.sprite = Resources.Load ("Image/masu", typeof(Sprite))as Sprite;
+//				GameObject obj = new GameObject ("Floor");
+//				SpriteRenderer spr = obj.AddComponent<SpriteRenderer> ();
+//				spr.sprite = Resources.Load ("Image/masu", typeof(Sprite))as Sprite;
 
-				obj.transform.position = new Vector3 (-7.0f + (i * 1), 0.0f, -5.0f+(j*1));
+				GameObject obj = MyUtility.CreateSprite (
+					                 FloorFile,
+					                 "Floor",
+					                 "Image/masu");
+
+				obj.transform.position = new Vector3 (-5.83f + (i * 1), 0.0f, -4.14f+(j*1));
 				obj.transform.eulerAngles = new Vector3 (90.0f, 0.0f, 0.0f);
-				obj.transform.localScale = new Vector3 (2.0f, 1.0f, 1.0f);
+				obj.transform.localScale = new Vector3 (2.0f, 2.1f, 1.0f);
 
-				//transform.SetParent ();
 
 			}
 
