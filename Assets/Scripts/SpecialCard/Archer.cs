a0f572c17785a6f696443d608160375b65fe4c49
mausya <aoisuisei3246@yahoo.co.jp>
2016-11-07 14:52:54 +0900

    特殊カードスクリプト製作中

---
+++ Assets/Scripts/SpecialCard/Archer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Archer : SpecialCard {
+
+	// Use this for initialization
+	public override void Start () {
+	
+	}
+
+    // Update is called once per frame
+    public override void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpecialCard/SpecialCard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialCard : MonoBehaviour {
+
+    public string m_howTo;
+	// Use this for initialization
+	public virtual void Start () {}
+	
+	// Update is called once per frame
+	public virtual void Update () {}
+}
