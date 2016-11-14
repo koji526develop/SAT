dd4fd53cc54f57124aa8209804ad817372450593
mausya <aoisuisei3246@yahoo.co.jp>
2016-11-14 17:36:28 +0900

    モブ出現　ほぼ実装

--- Assets/Scripts/SpecialCard/MobSpawner.cs
+++ Assets/Scripts/SpecialCard/MobSpawner.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class MobSpawner : SpecialCard
 {
 
-   
+
     private GameObject m_1stSelectColumn;
     private GameObject m_2ndSelectColumn;
 
     int GetPlayerID(GameObject _targetObject)
     {
 
-        return _targetObject.GetComponent<Character>().status.PlayerID;
+        return int.Parse(_targetObject.tag.Substring(_targetObject.tag.Length - 1));
     }
 
 
@@ -19,27 +19,60 @@ public class MobSpawner : SpecialCard
 
 
     // Update is called once per frame
-    public override void Update () {
+    public override void Update()
+    {
 
         if (m_1stSelectColumn == null)
         {
-            if(TouchManager.GetTouchInfo(0)== TouchInfo.Ended)
+            if (TouchManager.GetTouchInfo(0) == TouchInfo.Ended)
             {
-                 m_1stSelectColumn = TouchManager.GetRaycastHitObject(m_mainCamera, 0);
+                if (m_1stSelectColumn = TouchManager.GetRaycastHitObject(m_mainCamera, 0))
+                {
+
+                    if (m_1stSelectColumn.name != "Floor")
+                    {
+                        m_1stSelectColumn = null;
+                        return;
+                    }
+                }
             }
+
+            
         }
 
-        else if(m_2ndSelectColumn ==null)
+        else if (m_2ndSelectColumn == null)
         {
             if (TouchManager.GetTouchInfo(0) == TouchInfo.Ended)
             {
-                m_2ndSelectColumn = TouchManager.GetRaycastHitObject(m_mainCamera, 0);
+               if( m_2ndSelectColumn = TouchManager.GetRaycastHitObject(m_mainCamera, 0))
+                {
+                    if (m_2ndSelectColumn.name != "Floor")
+                    {
+                        m_2ndSelectColumn = null;
+                        return;
+                    }
+                }
             }
         }
 
-        if (m_1stSelectColumn != null && m_2ndSelectColumn != null)
-        {
-            Destroy(this);
+            if (m_1stSelectColumn != null && m_2ndSelectColumn != null)
+            {
+                Character character = Character.CreateObject(
+                GameObject.FindWithTag("BattleManager").transform,
+                Character.CharacterType.Sword,
+                Character.GetSpawnPosition(m_UsedPlayerID, GetPlayerID(m_1stSelectColumn)),
+                m_UsedPlayerID).GetComponent<Character>();
+                character.mapColumn = GetPlayerID(m_1stSelectColumn);
+
+                character = Character.CreateObject(
+                GameObject.FindWithTag("BattleManager").transform,
+                Character.CharacterType.Sword,
+                Character.GetSpawnPosition(m_UsedPlayerID, GetPlayerID(m_2ndSelectColumn)),
+                m_UsedPlayerID).GetComponent<Character>();
+                character.mapColumn = GetPlayerID(m_2ndSelectColumn);
+
+                Destroy(this);
+            }
         }
     }
-}
+
