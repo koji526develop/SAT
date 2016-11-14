96e8833101a901963d6facfcb2a4c9ae3ac5cf17
noan23435 <noan23435@gmail.com>
2016-11-15 01:02:47 +0900

    修正

--- Assets/Scripts/Main/CharacterNone.cs
+++ Assets/Scripts/Main/CharacterNone.cs
@@ -20,6 +20,22 @@ public class CharacterNone : State<Character>
 	}
 	bool NonHit(GameObject obj1,GameObject obj2)
 	{
+		// 衝突判定を行う距離
+		float distance = obj1.GetComponent<Character> ().status.attackDistance;
 
+		Character character1 = obj1.GetComponent<Character>();
+		Character character2 = obj2.GetComponent<Character>();
+
+		// キャラクターでなかったら抜ける
+		if (!obj2.CompareTag ("Character")) return false;
+
+		// 攻撃範囲に入ってなかったら抜ける
+		if (!(Mathf.Abs (obj1.transform.position.x - obj2.transform.position.x) < distance)) return false;
+
+
+		// 行が違ったら抜ける
+		if (character1.mapColumn != character2.mapColumn) return false;
+
+		return true;
 	}
 }
