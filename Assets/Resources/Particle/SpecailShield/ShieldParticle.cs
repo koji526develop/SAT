a86d6367e1a674cf40d0ebe1f5b54cd0dbce1048
renren-abc <nba.16.ren@gmail.com>
2016-11-25 22:52:21 +0900

    バリアパーティクル修正

---
+++ Assets/Resources/Particle/SpecailShield/ShieldParticle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//パーティクルが消えないようにいい感じにストップさせる
+public class ShieldParticle : MonoBehaviour
+{
+    ParticleSystem m_shieldParticle;
+    //何回も通さなくするためのもの
+    bool isStop;
+
+    void Awake()
+    {
+        //パーティクル取得
+        m_shieldParticle = this.GetComponent<ParticleSystem>();
+        isStop = true;
+    }
+
+	void Start ()
+    {
+
+	}
+
+	void Update ()
+    {
+        //パーティクルが消える前に停止する
+        if (m_shieldParticle.time >= 0.09f && isStop)
+        {
+            m_shieldParticle.Pause();
+            isStop = false;
+        }
+    }
+}
