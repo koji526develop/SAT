a9832b6b32c01e4342e24a9298cd605caecc08e2
koji526develop <koji@KojiOkada-no-MacBook-Pro.local>
2016-11-25 15:36:37 +0900

    特殊カード選択画面にカードの説明追加

---
+++ Assets/Scripts/SpecialCard/Cards/SpecialCard1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialCard1 : SpecialCard {
+
+	public static string m_howTo = "選択したライン上の兵士を消滅";
+	public static string m_imagePath = "path";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
