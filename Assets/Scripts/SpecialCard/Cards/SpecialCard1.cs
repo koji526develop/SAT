5d610e80307b42f5de22af115529bcb6683f73f1
renren-abc <nba.16.ren@gmail.com>
2016-11-28 18:47:53 +0900

    矢のパーティクル修正

--- Assets/Scripts/SpecialCard/Cards/SpecialCard1.cs
+++ Assets/Scripts/SpecialCard/Cards/SpecialCard1.cs
@@ -1,22 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpecialCard1 : SpecialCard {
+public class SpecialCard1 : SpecialCard
+{
 
-	public static string m_howTo = "選択したライン上の兵士を消滅";
-	public static string m_imagePath = "path";
+    public static string m_howTo = "選択したライン上の兵士を消滅";
+    public static string m_imagePath = "path";
 
     private int m_checkColumn = 0;
     GameUIManager m_gameUIManager;
-	// Use this for initialization
-	void Start () {
-        m_gameUIManager = gameManager.GetComponent<GameUIManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        for (int i=0;i<TouchManager.touchCount;i++)
+    GameObject m_ArrowObj;
+    float[] m_arrowPosZ = new float[6];
+    float m_arrowPosX;
+    ArrowMove arrowMove;
+
+    void Start()
+    {
+        m_gameUIManager = gameManager.GetComponent<GameUIManager>();
+        m_ArrowObj = GameObject.Instantiate(Resources.Load("Particle/SpecialArrow/SpecialArrow")) as GameObject;
+        m_ArrowObj.name = "IceArrow";
+        m_ArrowObj.transform.position = new Vector3(30000, -30000, 30000);
+        m_ArrowObj.AddComponent<ArrowMove>();
+        arrowMove = m_ArrowObj.GetComponent<ArrowMove>();
+
+        if (m_UsedPlayerID == 1) m_arrowPosX = MyUtility.SOLDIER_CREATE_LINE_X_1P;
+        else m_arrowPosX = MyUtility.SOLDIER_CREATE_LINE_X_2P;
+        for (int i = 0; i < 6; i++)
+        {
+            m_arrowPosZ[i] = 4 - (i * 2);
+        }
+    }
+
+    void Update()
+    {
+
+        for (int i = 0; i < TouchManager.touchCount; i++)
         {
             if (TouchManager.GetTouchInfo(i) == TouchInfo.Ended)
             {
@@ -25,6 +44,8 @@ public class SpecialCard1 : SpecialCard {
                     if (m_gameUIManager.TargetRect(TouchManager.GetTouchPosition(i), m_UsedPlayerID, j))
                     {
                         m_checkColumn = j;
+                        arrowMove.PlayerID = m_UsedPlayerID;
+                        m_ArrowObj.transform.position = new Vector3(m_arrowPosX, 0, m_arrowPosZ[m_checkColumn - 1]);
                         break;
                     }
 
@@ -32,7 +53,8 @@ public class SpecialCard1 : SpecialCard {
             }
         }
 
-        if (m_checkColumn != 0) {
+        if (m_checkColumn != 0)
+        {
 
             foreach (Transform childObj in battleManager)
             {
@@ -44,12 +66,11 @@ public class SpecialCard1 : SpecialCard {
                     {
                         //対象を削除
                         Destroy(childObj.gameObject);
-       
+
                     }
                 }
             }
             Destroy(this);
         }
-        
-	}
+    }
 }
