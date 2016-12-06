c0bb448885a9244852559da439f8543bda5addec
renren-abc <nba.16.ren@gmail.com>
2016-12-06 14:59:33 +0900

    タップエフェクト修正

--- Assets/Scripts/Lib/TapEffect.cs
+++ Assets/Scripts/Lib/TapEffect.cs
@@ -17,13 +17,19 @@ public class TapEffect : MonoBehaviour
 
     void Update()
     {
-		for (int i = 0; i < TouchManager.touchCount; i++) {
+        TouchManager.Update();
+
+        for (int i = 0; i < TouchManager.touchCount; i++)
+        {
+            Debug.Log(i);
 			TouchInfo touch = TouchManager.GetTouchInfo (i);
-			if (touch == TouchInfo.Began) {
+			if (touch == TouchInfo.Began)
+            {
 				var touchPos = m_camera.ScreenToWorldPoint (TouchManager.GetTouchPosition (i) + m_camera.transform.forward * 10);
 				m_tapEffect.transform.position = touchPos;
 				m_tapEffect.Emit (1);
-			}
+                Debug.Log("//タップパーティクル");
+            }
 		}
     }
 }
