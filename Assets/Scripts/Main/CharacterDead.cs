d6afa29e13ed1042eaefca242cddb576bddc5956
mausya <aoisuisei3246@yahoo.co.jp>
2016-12-13 01:37:26 +0900

    デストロイ時得点表示

--- Assets/Scripts/Main/CharacterDead.cs
+++ Assets/Scripts/Main/CharacterDead.cs
@@ -12,7 +12,17 @@ public class CharacterDead : State<Character>
 		Debug.Log("死んだ");
 		AudioManager.m_instance.PlaySE ("Vanish_SE");
 		m_instance.Destroy ();
-	}
+
+        if (this.m_instance.status.PlayerID == 1) {
+            GameObject.Find("GameManager").GetComponent<ScoreManager>().DirectGetPoint(2, 5, this.m_instance.transform.position);
+        }
+        else
+        {
+            GameObject.Find("GameManager").GetComponent<ScoreManager>().DirectGetPoint(1, 5, this.m_instance.transform.position);
+
+        }
+
+    }
 
 	public override void Update()
 	{
